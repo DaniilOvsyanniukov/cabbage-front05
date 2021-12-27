c6f7389a0ad8b05f1d7cbab66b47626781e3ef25
Даниил Овсянников <ovsuanikov@gmail.com>
2021-12-27 16:57:48 +0200

    fix

--- src/redux/Brief/brief-operations.js
+++ src/redux/Brief/brief-operations.js
@@ -9,43 +9,6 @@ import {
 import moment from 'moment';
 import axios from 'axios';
 
-function sortedData(dataperYear) {
-  if (dataperYear=== undefined || dataperYear.length === 0){return}
-  let sorted = [];
-  dataperYear.forEach(transactions => {
-
-    const findMonth = sorted.find((month, index, array) => {
-      if (month.month === transactions.month) {
-        return true
-      } else {
-        return false
-      }
-    })
-
-    if (findMonth === undefined) {
-        const newMonth = new Object();
-        newMonth.month = transactions.month;
-        newMonth.sum = transactions.sum;
-        sorted.push(newMonth)
-    } else {
-      const index = sorted.findIndex((month) => {
-           if (month.month === transactions.month) {
-        return true
-      } else {
-        return false
-        }
-      })
-        const editToMonth = new Object();
-        editToMonth.month = transactions.month;
-        editToMonth.sum = Number(transactions.sum) + Number(findMonth.sum);
-        sorted.splice(index, 1, editToMonth)
-
-    }
-
-  });
-  return sorted
-}
-
 const fatchTransactionsPerYear = ()=> async dispatch => {
   dispatch(fatchIncomeBriefPerYearRequest());
   dispatch(fatchExpenseBriefPerYearRequest());
@@ -66,12 +29,9 @@ const fatchTransactionsPerYear = ()=> async dispatch => {
         year,
       }
     });
-    const sortedExpense = sortedData(expenseData.data);
-    const sortedIncome = sortedData(incomeData.data);
-    console.log(sortedExpense)
-    console.log(sortedIncome)
-    dispatch(fatchIncomeBriefPerYearSuccess(sortedExpense));
-    dispatch(fatchExpenseBriefPerYearSuccess(sortedIncome));
+
+    dispatch(fatchIncomeBriefPerYearSuccess(incomeData.data));
+    dispatch(fatchExpenseBriefPerYearSuccess(expenseData.data));
   } catch (error) {
     dispatch(fatchIncomeBriefPerYearError(error));
     dispatch(fatchExpenseBriefPerYearError(error));
--- src/redux/Brief/brief-reduser.js
+++ src/redux/Brief/brief-reduser.js
@@ -10,6 +10,43 @@ import {
 } from './brief-action';
 
 
+const sortedData =(dataperYear)=> {
+  if (dataperYear=== undefined || dataperYear.length === 0){return}
+  let sorted = [];
+  dataperYear.forEach(transactions => {
+
+    const findMonth = sorted.find((month, index, array) => {
+      if (month.month === transactions.month) {
+        return true
+      } else {
+        return false
+      }
+    })
+
+    if (findMonth === undefined) {
+        const newMonth = new Object();
+        newMonth.month = transactions.month;
+        newMonth.sum = transactions.sum;
+        sorted.push(newMonth)
+    } else {
+      const index = sorted.findIndex((month) => {
+           if (month.month === transactions.month) {
+        return true
+      } else {
+        return false
+        }
+      })
+        const editToMonth = new Object();
+        editToMonth.month = transactions.month;
+        editToMonth.sum = Number(transactions.sum) + Number(findMonth.sum);
+        sorted.splice(index, 1, editToMonth)
+
+    }
+
+  });
+  return sorted
+}
+
 
 const isLoading = createReducer(false, {
   [fatchIncomeBriefPerYearRequest]: () => true,
@@ -21,11 +58,11 @@ const isLoading = createReducer(false, {
 });
 
 const expensePerYrarReducer = createReducer([], {
-  [fatchIncomeBriefPerYearSuccess]: (_, { payload }) => payload,
+  [fatchIncomeBriefPerYearSuccess]: (_, { payload }) => { return sortedData(payload.data) },
 });
 
 const incomePerYearReducer = createReducer([], {
-  [fatchExpenseBriefPerYearSuccess]: (_, { payload }) => payload,
+  [fatchExpenseBriefPerYearSuccess]: (_, { payload }) => { return sortedData(payload.data) },
 });
 
 
